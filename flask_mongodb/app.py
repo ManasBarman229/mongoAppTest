230882854dff2d485849a117a6d0c5301f0383c8
Manas Barman <manasbarman229@gmail.com>
2022-09-05 01:09:26 +0530

    added mongodb atlas config and updated requirement

--- flask_mongodb/app.py
+++ flask_mongodb/app.py
@@ -1,18 +1,32 @@
+import urllib.parse
 from flask import Flask, request, render_template
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
 
-# define the mongodb client
-client = MongoClient(port=27017)
 
-# define the database to use
-db = client.student_data
+# from pymongo import MongoClient
+# # define the mongodb client
+# client = MongoClient(port=27017)
 
+# # define the database to use
+# db = client.student_data
 # define the flask app
 app = Flask(__name__)
 
+
+# mongodb atlas config
+
+username = urllib.parse.quote_plus('Manas_database')
+password = urllib.parse.quote_plus("@qwerty@123@")
+app.config["SECRET_KEY"] = "d10b6b145a28543bd613eecd0d4e6fdec14a8cc5"
+app.config["MONGO_URI"] = "mongodb+srv://{}:{}@cluster0.fzqzl4h.mongodb.net /?retryWrites = true & w = majority".format(
+    username, password)
+
+# setup mongodb
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
+
+
 # define the home page route
-
-
 @app.route('/')
 def home():
     return render_template("index.html")
