66b13c7a4a6c30813fa5ecc9d8e10dafb5bdbe9a
Manas Barman <manasbarman229@gmail.com>
2022-09-05 13:20:42 +0530

    removed unnecessary code lines from app.py file

--- flask_mongodb/app.py
+++ flask_mongodb/app.py
@@ -1,35 +1,17 @@
 from pymongo import MongoClient
-# import urllib.parse
 from flask import Flask, request, render_template
-# from flask_pymongo import PyMongo
 
 
 # define the flask app
 app = Flask(__name__)
 
 
-# # define the mongodb client
-# client = MongoClient(port=27017)
+# define the mongodb client
 
 client = MongoClient(
     "mongodb+srv://Manas_database:%40qwerty%40123@dataset.xni2nb8.mongodb.net/test")
+# define the database to use
 db = client.projectData
-
-# # define the database to use
-# db = client.student_data
-
-
-# mongodb atlas config
-
-# username = urllib.parse.quote_plus('Manas_database')
-# password = urllib.parse.quote_plus("@qwerty@123@")
-# app.config["SECRET_KEY"] = "d10b6b145a28543bd613eecd0d4e6fdec14a8cc5"
-# app.config["MONGO_URI"] = "mongodb+srv://{}:{}@cluster0.fzqzl4h.mongodb.net /?retryWrites = true & w = majority".format(
-#     username, password)
-
-# # setup mongodb
-# mongodb_client = PyMongo(app)
-# db = mongodb_client.db
 
 
 # define the home page route
